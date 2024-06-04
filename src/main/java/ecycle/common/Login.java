ecdfbe2c480811957fda3536f0d78a98d14cc3d6
Antox003 <antonio03.margio@gmail.com>
2024-06-04 11:45:18 +0200

    change login system

--- src/main/java/ecycle/common/Login.java
+++ src/main/java/ecycle/common/Login.java
@@ -1,16 +1,16 @@
 package ecycle.common;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
 
 /**
  * Servlet implementation class Login
@@ -37,40 +37,55 @@ public class Login extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
 		List<String> errors = new ArrayList<>();
-		RequestDispatcher dispatcherToLoginPage = request.getRequestDispatcher("common/login.jsp");
-		
-		if(username == null || username.trim().isEmpty()) {
-			errors.add("Il campo non può essere vuoto!");
+		RequestDispatcher dispatcherToLoginPage = request.getRequestDispatcher("login.jsp");
+		if (username == null || username.trim().isEmpty()) {
+			errors.add("Il campo username non può essere vuoto!");
 		}
-		if(password == null || password.trim().isEmpty()) {
-			errors.add("Il campo non puoò essere vuoto!");
+		if (password == null || password.trim().isEmpty()) {
+			errors.add("Il campo password non può essere vuoto!");
 		}
-		
-		if(!errors.isEmpty()) {
+		if (!errors.isEmpty()) {
 			request.setAttribute("errors", errors);
 			dispatcherToLoginPage.forward(request, response);
-			return;
+			return; // note the return statement here!!!
 		}
-		
 		username = username.trim();
 		password = password.trim();
-		
-		if(username.equals("admin") && password.equals("mypass")) {
-			request.getSession().setAttribute("isAdmin", Boolean.TRUE);
+		String hashPassword = toHash(password);
+		// Hash of "mypass":
+		String hashPasswordToBeMatch = 
+				"1c573dfeb388b562b55948af954a7b344dde1cc2099e978a992790429e7c01a4205506a93d9aef3bab32d6f06d75b7777a7ad8859e672fedb6a096ae369254d2";
+		if (username.equals("admin") && hashPassword.equals(hashPasswordToBeMatch)) { // admin
+			request.getSession().setAttribute("isAdmin", Boolean.TRUE); // inserisco il token nella sessione
 			response.sendRedirect("admin/protected.jsp");
-		} else if (username.equals("user") && password.equals("mypass")) {
-			request.getSession().setAttribute("isAdmin", Boolean.FALSE);
-			response.sendRedirect("common/home.jsp");
+		} else if (username.equals("user") && hashPassword.equals(hashPasswordToBeMatch)) { // user
+			request.getSession().setAttribute("isAdmin", Boolean.FALSE); // inserisco il token nella sessione
+			response.sendRedirect("common/protected.jsp");
 		} else {
 			errors.add("Username o password non validi!");
 			request.setAttribute("errors", errors);
 			dispatcherToLoginPage.forward(request, response);
 		}
+	}
+
+	private String toHash(String password) {
+		String hashString = null;
+		try {
+			java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-512");
+			byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+			hashString = "";
+			for (int i = 0; i < hash.length; i++) {
+				hashString += Integer.toHexString((hash[i] & 0xFF) | 0x100).substring(1, 3);
+			}
+		} catch (java.security.NoSuchAlgorithmException e) {
+			System.out.println(e);
+		}
+		return hashString;
 	}
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -6,6 +6,14 @@
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
 </head>
+<%
+// Check user credentials
+Boolean isAdmin = (Boolean) session.getAttribute("isAdmin");
+if (isAdmin == null){	
+    response.sendRedirect(request.getContextPath() + "/login.jsp"); 
+    return;
+}
+%>
 <body>
 	
 	<h1>HOME</h1>
